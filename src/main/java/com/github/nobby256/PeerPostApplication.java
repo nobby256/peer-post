2a50ab695c605bdcdde584871f37f8edef038e33
nobby256 <helloworld_1024@yahoo.co.jp>
2020-01-06 02:36:42 +0900

    更新

---
+++ src/main/java/com/github/nobby256/PeerPostApplication.java
@@ -0,0 +1,13 @@
+package com.github.nobby256;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nobby256/PeerPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nobby256;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
